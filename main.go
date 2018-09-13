641060fa888a9bca075a26007fdc7410e2c90d0f
8845musign <8845musign@gmail.com>
2018-09-14 00:28:27 +0900

    feat: add basic auth

--- main.go
+++ main.go
@@ -4,10 +4,19 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
 
 func main() {
 	e := echo.New()
+
+	e.Use(middleware.BasicAuth(func(username, password string, c echo.Context) (bool, error) {
+		if username == "joe" && password == "secret" {
+			return true, nil
+		}
+		return false, nil
+	}))
+
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
